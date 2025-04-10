fb121974f8b4fabb5b8bed0e972fbdfefa89c0c6
McYum <derpingstones@gmail.com>
2025-04-10 20:34:44 +0200

    init commit

--- src/main/java/org/example/demo3/HelloApplication.java
+++ src/main/java/org/example/demo3/HelloApplication.java
@@ -2,22 +2,25 @@ package org.example.demo3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/demo3/hello-view.fxml"));
+        Parent root = loader.load();
+
+        HelloController controller = loader.getController();
+        controller.initializeGame();
+
+        primaryStage.setTitle("Gwent Card Game");
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
